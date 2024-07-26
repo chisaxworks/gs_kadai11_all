dcd03d6f492edad1626625b3a0f5af01c1dbdf70
chisaxworks <chisaxworks@gmail.com>
2024-07-26 10:04:10 +0900

    Sqlファイル更新

--- sql/chisaxworks_gs_kadai_php.sql
+++ sql/chisaxworks_gs_kadai_php.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- ホスト: localhost
--- 生成日時: 2024 年 7 月 23 日 16:30
--- サーバのバージョン： 10.4.28-MariaDB
--- PHP のバージョン: 8.2.4
+-- ホスト: mysql635.db.sakura.ne.jp
+-- 生成日時: 2024 年 7 月 26 日 10:02
+-- サーバのバージョン： 5.7.40-log
+-- PHP のバージョン: 8.2.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- データベース: `gs_app_prot1`
+-- データベース: `chisaxworks_gs_kadai_php`
 --
 
 -- --------------------------------------------------------
@@ -36,10 +36,10 @@ CREATE TABLE `event` (
   `event_openTime` time NOT NULL,
   `event_members` varchar(500) NOT NULL,
   `event_source` varchar(30) NOT NULL,
-  `isActive` int(1) NOT NULL DEFAULT 1,
+  `isActive` int(1) NOT NULL DEFAULT '1',
   `createdDate` datetime NOT NULL,
   `modifiedDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- テーブルのデータのダンプ `event`
@@ -51,7 +51,12 @@ INSERT INTO `event` (`event_id`, `event_name`, `venue_id`, `event_startDate`, `e
 (3, '令和ロマンのR2D2', 1, '2024-09-08', '19:00:00', '18:30:00', '令和ロマン', 'FANY', 1, '2024-07-22 17:13:14', '2024-07-22 17:13:14'),
 (4, 'まんざい道', 2, '2024-08-04', '20:00:00', '19:40:00', 'ダイタク／ゆにばーす／カラタチ／令和ロマン／素敵じゃないか／エバース／ナイチンゲールダンス／他', 'FANY', 1, '2024-07-22 17:14:57', '2024-07-22 17:14:57'),
 (5, 'シシガシラ×ダイヤモンド×黒帯×チェリー大作戦『漫才至上主義』', 2, '2024-08-17', '18:00:00', '17:40:00', 'シシガシラ／ダイヤモンド／黒帯／チェリー大作戦／ゲスト:シンクロニシティ', 'FANY', 1, '2024-07-22 17:16:15', '2024-07-22 17:16:15'),
-(6, 'シゲカズですpresentsクセツアーさらにさらにルミネザ～神豪華全組ネタと大阪名物神クセ企画の日～', 1, '2024-08-13', '19:30:00', '19:00:00', 'シゲカズです／ジェラードン(アタック西本・かみちぃ)／ロングコートダディ／マユリカ／ケビンス／エルフ', 'FANY', 1, '2024-07-22 17:19:28', '2024-07-22 17:19:28');
+(6, 'シゲカズですpresentsクセツアーさらにさらにルミネザ～神豪華全組ネタと大阪名物神クセ企画の日～', 1, '2024-08-13', '19:30:00', '19:00:00', 'シゲカズです／ジェラードン(アタック西本・かみちぃ)／ロングコートダディ／マユリカ／ケビンス／エルフ', 'FANY', 1, '2024-07-22 17:19:28', '2024-07-22 17:19:28'),
+(7, 'ルミネtheよしもと 平日14時の部 プラス', 1, '2024-09-27', '14:00:00', '13:30:00', '佐久間一行／トータルテンボス／NON STYLE／ライス／チョコレートプラネット／霜降り明星／コロコロチキチキペッパーズ／マユリカ／他', 'FANY', 1, '2024-07-24 17:37:47', '2024-07-24 17:37:47'),
+(8, 'ルミネtheよしもと 平日16時の部 プラス', 1, '2024-09-27', '16:00:00', '15:30:00', '佐久間一行／トータルテンボス／NON STYLE／ライス／チョコレートプラネット／霜降り明星／コロコロチキチキペッパーズ／マユリカ／他', 'FANY', 1, '2024-07-24 17:38:10', '2024-07-24 17:38:10'),
+(9, '濁', 2, '2024-08-29', '21:00:00', '20:40:00', 'ニッポンの社長／ロングコートダディ／シカゴ実業／大自然／マユリカ／kento fukaya／シモリュウ／ヒューマン中村／めっちゃ', 'FANY', 1, '2024-07-24 17:43:12', '2024-07-24 17:43:12'),
+(10, ' ヨシモト∞ホール特別公演「ワラムゲ!SP」', 2, '2024-09-23', '15:00:00', '14:40:00', 'マユリカ／ネルソンズ／ゆにばーす／アイロンヘッド／THIS IS パン／他', 'FANY', 1, '2024-07-24 17:45:27', '2024-07-24 17:45:27'),
+(11, '過去公演', 2, '2024-07-10', '19:00:00', '18:00:00', 'ロングコートダディ／マユリカ／令和ロマン', 'FANY', 1, '2024-07-25 19:51:05', '2024-07-25 19:51:05');
 
 -- --------------------------------------------------------
 
@@ -66,7 +71,7 @@ CREATE TABLE `event_mg_tbl` (
   `undermg_flg` int(11) NOT NULL,
   `createdDate` datetime NOT NULL,
   `modifiedDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- テーブルのデータのダンプ `event_mg_tbl`
@@ -92,7 +97,7 @@ CREATE TABLE `favorite` (
   `user_id` int(12) NOT NULL,
   `createdDate` datetime NOT NULL,
   `modifiedDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- テーブルのデータのダンプ `favorite`
@@ -109,43 +114,6 @@ INSERT INTO `favorite` (`ent_id`, `ent_name`, `ent_type`, `user_id`, `createdDat
 -- --------------------------------------------------------
 
 --
--- テーブルの構造 `o_entertainer`
---
-
-CREATE TABLE `o_entertainer` (
-  `ent_id` int(12) NOT NULL,
-  `ent_name` varchar(255) NOT NULL,
-  `ent_type` varchar(30) NOT NULL,
-  `createdDate` datetime NOT NULL,
-  `modifiedDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- テーブルのデータのダンプ `o_entertainer`
---
-
-INSERT INTO `o_entertainer` (`ent_id`, `ent_name`, `ent_type`, `createdDate`, `modifiedDate`) VALUES
-(1, 'ロングコートダディ', '芸人', '2024-07-21 17:54:00', '2024-07-21 17:54:00'),
-(2, 'マユリカ', '芸人', '2024-07-21 17:54:10', '2024-07-21 17:54:10'),
-(3, 'かまいたち', '芸人', '2024-07-22 11:10:01', '2024-07-22 11:10:01');
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `o_event_ent_tbl`
---
-
-CREATE TABLE `o_event_ent_tbl` (
-  `event_ent_id` int(12) NOT NULL,
-  `event_id` int(12) NOT NULL,
-  `ent_id` int(12) NOT NULL,
-  `createdDate` datetime NOT NULL,
-  `modifiedDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- テーブルの構造 `sales_methods`
 --
 
@@ -154,7 +122,7 @@ CREATE TABLE `sales_methods` (
   `sm_name` varchar(30) NOT NULL,
   `createdDate` datetime NOT NULL,
   `modifiedDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- テーブルのデータのダンプ `sales_methods`
@@ -176,7 +144,7 @@ CREATE TABLE `ticket_agency` (
   `agency_url` varchar(2100) NOT NULL,
   `createdDate` datetime NOT NULL,
   `modifiedDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- テーブルのデータのダンプ `ticket_agency`
@@ -202,7 +170,7 @@ CREATE TABLE `ticket_info` (
   `ti_endTime` time NOT NULL,
   `createdDate` datetime NOT NULL,
   `modifiedDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- テーブルのデータのダンプ `ticket_info`
@@ -211,7 +179,20 @@ CREATE TABLE `ticket_info` (
 INSERT INTO `ticket_info` (`ti_id`, `event_id`, `sm_id`, `agency_id`, `ti_startDate`, `ti_startTime`, `ti_endDate`, `ti_endTime`, `createdDate`, `modifiedDate`) VALUES
 (1, 1, 1, 1, '2024-07-18', '10:00:00', '2024-08-16', '17:30:00', '2024-07-23 10:22:26', '2024-07-23 10:22:26'),
 (2, 3, 1, 1, '2024-08-09', '10:00:00', '2024-09-08', '17:00:00', '2024-07-23 10:27:26', '2024-07-23 10:27:26'),
-(3, 3, 2, 1, '2024-08-01', '11:00:00', '2024-08-06', '11:00:00', '2024-07-23 10:28:17', '2024-07-23 10:28:17');
+(3, 3, 2, 1, '2024-08-01', '11:00:00', '2024-08-06', '11:00:00', '2024-07-23 10:28:17', '2024-07-23 10:28:17'),
+(4, 7, 2, 1, '2024-07-25', '11:00:00', '2024-07-28', '11:00:00', '2024-07-24 17:29:28', '2024-07-24 17:29:28'),
+(5, 2, 1, 1, '2024-07-06', '10:00:00', '2024-08-28', '17:30:00', '2024-07-24 17:30:47', '2024-07-24 17:30:47'),
+(6, 4, 1, 1, '2024-07-05', '10:00:00', '2024-08-03', '23:59:00', '2024-07-24 17:32:33', '2024-07-24 17:32:33'),
+(7, 5, 1, 1, '2024-07-15', '10:00:00', '2024-08-16', '23:59:00', '2024-07-24 17:33:28', '2024-07-24 17:33:28'),
+(8, 6, 1, 1, '2024-05-19', '10:00:00', '2024-08-13', '17:30:00', '2024-07-24 17:34:35', '2024-07-24 17:34:35'),
+(9, 8, 2, 1, '2024-07-25', '11:00:00', '2024-07-28', '11:00:00', '2024-07-24 17:39:10', '2024-07-24 17:39:10'),
+(10, 8, 1, 1, '2024-08-05', '10:00:00', '2024-09-27', '12:00:00', '2024-07-24 17:39:53', '2024-07-24 17:39:53'),
+(11, 9, 2, 1, '2024-07-25', '11:00:00', '2024-07-28', '11:00:00', '2024-07-24 17:39:10', '2024-07-24 17:39:10'),
+(12, 9, 1, 1, '2024-08-05', '10:00:00', '2024-09-27', '12:00:00', '2024-07-24 17:39:53', '2024-07-24 17:39:53'),
+(13, 10, 1, 1, '2024-07-25', '10:00:00', '2024-08-28', '23:59:00', '2024-07-24 17:44:36', '2024-07-24 17:44:36'),
+(14, 11, 2, 1, '2024-07-25', '11:00:00', '2024-07-28', '11:00:00', '2024-07-24 17:45:59', '2024-07-24 17:45:59'),
+(15, 11, 1, 1, '2024-08-05', '10:00:00', '2024-09-22', '23:59:00', '2024-07-24 17:46:25', '2024-07-24 17:46:25'),
+(16, 2, 2, 1, '2024-07-11', '11:00:00', '2024-07-16', '11:00:00', '2024-07-25 19:53:06', '2024-07-25 19:53:06');
 
 -- --------------------------------------------------------
 
@@ -224,10 +205,10 @@ CREATE TABLE `user` (
   `user_name` varchar(30) NOT NULL,
   `user_email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `isActive` int(1) NOT NULL DEFAULT 1,
+  `isActive` int(1) NOT NULL DEFAULT '1',
   `createdDate` datetime NOT NULL,
   `modifiedDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- テーブルのデータのダンプ `user`
@@ -250,7 +231,7 @@ CREATE TABLE `venue` (
   `venue_pref` varchar(4) NOT NULL,
   `createdDate` datetime NOT NULL,
   `modifiedDate` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- テーブルのデータのダンプ `venue`
@@ -285,20 +266,6 @@ ALTER TABLE `event_mg_tbl`
 ALTER TABLE `favorite`
   ADD PRIMARY KEY (`ent_id`),
   ADD KEY `user_id` (`user_id`);
-
---
--- テーブルのインデックス `o_entertainer`
---
-ALTER TABLE `o_entertainer`
-  ADD PRIMARY KEY (`ent_id`);
-
---
--- テーブルのインデックス `o_event_ent_tbl`
---
-ALTER TABLE `o_event_ent_tbl`
-  ADD PRIMARY KEY (`event_ent_id`),
-  ADD KEY `event_id` (`event_id`),
-  ADD KEY `ent_id` (`ent_id`);
 
 --
 -- テーブルのインデックス `sales_methods`
@@ -342,7 +309,7 @@ ALTER TABLE `venue`
 -- テーブルの AUTO_INCREMENT `event`
 --
 ALTER TABLE `event`
-  MODIFY `event_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `event_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- テーブルの AUTO_INCREMENT `event_mg_tbl`
@@ -355,18 +322,6 @@ ALTER TABLE `event_mg_tbl`
 --
 ALTER TABLE `favorite`
   MODIFY `ent_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- テーブルの AUTO_INCREMENT `o_entertainer`
---
-ALTER TABLE `o_entertainer`
-  MODIFY `ent_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- テーブルの AUTO_INCREMENT `o_event_ent_tbl`
---
-ALTER TABLE `o_event_ent_tbl`
-  MODIFY `event_ent_id` int(12) NOT NULL AUTO_INCREMENT;
 
 --
 -- テーブルの AUTO_INCREMENT `sales_methods`
@@ -384,7 +339,7 @@ ALTER TABLE `ticket_agency`
 -- テーブルの AUTO_INCREMENT `ticket_info`
 --
 ALTER TABLE `ticket_info`
-  MODIFY `ti_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ti_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- テーブルの AUTO_INCREMENT `user`
@@ -420,13 +375,6 @@ ALTER TABLE `event_mg_tbl`
 --
 ALTER TABLE `favorite`
   ADD CONSTRAINT `favorite_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE;
-
---
--- テーブルの制約 `o_event_ent_tbl`
---
-ALTER TABLE `o_event_ent_tbl`
-  ADD CONSTRAINT `o_event_ent_tbl_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `o_event_ent_tbl_ibfk_2` FOREIGN KEY (`ent_id`) REFERENCES `o_entertainer` (`ent_id`) ON UPDATE CASCADE;
 
 --
 -- テーブルの制約 `ticket_info`
